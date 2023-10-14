6ee583d2ee4aa56c358fe4cdf565b8c3fb700165
ABDURRAHMAN SAID GURBUZ <e2380483@ceng.metu.edu.tr>
2023-10-14 19:50:27 +0300

    tester for the model is added

--- tester.py
+++ tester.py
@@ -1,4 +1,5 @@
 from model_try import BPNet
+from model_try import custom_loss
 import torch
 import torch.nn.functional as F
 
@@ -11,9 +12,23 @@ def test_bpnet_model():
 
     model = BPNet(num_tasks)
 
-    dna_seq = torch.randint(num_channels, (batch_size * sequence_length, ))
+    dna_seq = torch.randint(num_channels, (batch_size * sequence_length,))
     dna_batch = F.one_hot(dna_seq, num_classes=num_channels).type(torch.FloatTensor)
-    outputs = model(dna_batch.unsqueeze(0).view(batch_size, sequence_length, num_channels).permute(0, 2, 1))
-    assert outputs[0].shape == (batch_size, sequence_length, 2) # profile head output
-    assert outputs[1].shape == (batch_size, 2) # total counts head output
+    outputs = model(
+        dna_batch.unsqueeze(0)
+        .view(batch_size, sequence_length, num_channels)
+        .permute(0, 2, 1)
+    )
+    assert outputs[0].shape == (batch_size, sequence_length, 2)  # profile head output
+    assert outputs[1].shape == (batch_size, 2)  # total counts head output
+
+    targets = []
+    for i in range(num_tasks * 2):
+        if i % 2 == 0:
+            targets.append(torch.randint(2, (batch_size, sequence_length, 2)))
+        else:
+            targets.append(torch.rand(batch_size, 2))
+    loss = custom_loss(outputs=outputs, targets=targets)
+    loss.backward()
+    assert loss > 0.0
     return
