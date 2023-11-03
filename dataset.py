313e3d513f98c5e61c8209584b03ec33ee915c7d
ABDURRAHMAN SAID GURBUZ <e2380483@ceng.metu.edu.tr>
2023-11-03 16:04:31 +0300

    custom dataset is updated to get sequences from multiple chromosomes

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 import torch
 from torch.utils.data import Dataset
 from helpers import one_hot_encoder
+from helpers import seq_to_one_hot
 
 
 class DNADataset(Dataset):
@@ -11,13 +12,27 @@ class DNADataset(Dataset):
         self.num_tasks = num_tasks
 
     def __len__(self):
-        return len(self.inputs)
+        total_samples = 0
+        for chr in self.inputs:
+            total_samples += len(chr.seq) // self.sequence_length
+        return total_samples
 
     def __getitem__(self, idx):
+
+        # get the current chromosome
+        chr_idx = 0
+        offset = idx * self.sequence_length
+        while offset >= len(self.inputs[chr_idx].seq):
+            offset -= len(self.inputs[chr_idx].seq)
+            chr_idx += 1
+        chr = self.inputs[chr_idx]
+
         start = idx * self.sequence_length
         end = start + self.sequence_length
-        input = one_hot_encoder(str(self.inputs[start:end]))
+        input = seq_to_one_hot(str(chr[start:end]))
         # TODO will configure getting target (actually multiple target since we have multiple tasks and multiple head)
+        
+        
         target = self.targets[idx]
         input_tensor = torch.tensor(input, dtype=torch.float32)
         target_tensor = torch.tensor(target, dtype=torch.float32)
