9309a1a5bf46cbd7ac1c3cfa40f6ed17b81c2018
ABDURRAHMAN SAID GURBUZ <e2380483@ceng.metu.edu.tr>
2023-11-03 16:06:24 +0300

    data loading is implemented

--- train.py
+++ train.py
@@ -5,16 +5,19 @@ from helpers import custom_loss
 from torch.utils.data import DataLoader
 import torch
 import torch.optim as optim
+from Bio import SeqIO
+from Bio import Seq
+import pyBigWig
 
 
-def train(data_loader, model, num_epochs, optimizer, loss_fn, validation_loader=None):
-    if validation_loader:
+def train(train_loader, model, num_epochs, optimizer, loss_fn, val_loader=None):
+    if val_loader:
         patience = 5
         best_val_loss = float("inf")
         counter = 0
     for epoch in range(num_epochs):
         total_loss = 0.0
-        for i, data in enumerate(data_loader):
+        for i, data in enumerate(train_loader):
             inputs, targets = data
 
             optimizer.zero_grad()
@@ -28,11 +31,11 @@ def train(data_loader, model, num_epochs, optimizer, loss_fn, validation_loader=
 
         avg_loss = total_loss / (i + 1)
 
-        if validation_loader:
+        if val_loader:
             model.eval()
             total_val_loss = 0.0
             with torch.no_grad():
-                for index, data in enumerate(validation_loader):
+                for index, data in enumerate(val_loader):
                     inputs, targets = data
                     outputs = model(inputs)
                     loss = loss_fn(outputs, targets)
@@ -69,16 +72,54 @@ if __name__ == "__main__":
     num_epochs = 100
     learning_rate = 0.004
 
+    tasks = ['Oct4', 'Sox2', 'Nanog', 'Klf4']
     # TODO
-    # dna_sequences = ...  will be implemented
-    # targets = ... will be implemented
+    choromosomes = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 
+                    'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14',
+                    'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chrX', 'chrY']
+    
+    train_chrs = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8',
+                  'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15']
+    val_chrs = ['chr16', 'chr17', 'chr18']
+    test_chrs = ['chr19', 'chrX', 'chrY']
 
-    dna_dataset = DNADataset(dna_sequences, targets, sequence_length, num_tasks)
-    data_loader = DataLoader(dna_dataset, batch_size=batch_size, shuffle=True)
+    dna_file = 'mm10.fa'
+
+    # get labels
+    pos_counts = []
+    neg_counts = []
+    for task in tasks:
+        pos_counts.append(pyBigWig.open(f'{task}_counts.pos.bw'))
+        neg_counts.append(pyBigWig.open(f'{task}_counts.neg.bw'))
+
+    # dna_sequences = ...  will be implemented
+    with open(dna_file, 'r') as file:
+        dna = list(SeqIO.parse(file, 'fasta'))
+    # dna_sequences = Seq('')
+    # with open(dna_file, 'r') as dna:
+    #     for sequence in SeqIO.parse(dna, 'fasta'):
+    #         dna_sequences += sequence.seq
+    train_sequences = list(filter(lambda c: c.id in train_chrs, dna))
+    val_sequences = list(filter(lambda c: c.id in val_chrs, dna))
+    test_sequences = list(filter(lambda c: c.id in test_chrs, dna))
+
+    # targets = ... will be implemented
+    train_targets = (train_chrs, pos_counts, neg_counts)
+    val_targets = (val_chrs, pos_counts, neg_counts)
+    test_targets = (test_chrs, pos_counts, neg_counts)
+
+    train_dataset = DNADataset(train_sequences, train_targets, sequence_length, num_tasks)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+    val_dataset = DNADataset(train_sequences, val_targets, sequence_length, num_tasks)
+    val_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
+
+    test_dataset = DNADataset(train_sequences, test_targets, sequence_length, num_tasks)
+    test_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
 
     model = BPNet(num_tasks)
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     loss_fn = custom_loss
 
     # train
-    train(data_loader, model, num_epochs, optimizer, loss_fn)
+    train(train_loader, model, num_epochs, optimizer, loss_fn, val_loader)
