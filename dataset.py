4ffa4e654eaee13a4bd06170e42fc23ae4490be1
ABDURRAHMAN SAID GURBUZ <e2380483@ceng.metu.edu.tr>
2023-11-07 17:08:19 +0300

    added getting target

--- dataset.py
+++ dataset.py
@@ -27,13 +27,26 @@ class DNADataset(Dataset):
             chr_idx += 1
         chr = self.inputs[chr_idx]
 
-        start = idx * self.sequence_length
+        start = offset
         end = start + self.sequence_length
-        input = seq_to_one_hot(str(chr[start:end]))
+        input = seq_to_one_hot(str(chr.seq[start:end]))
         # TODO will configure getting target (actually multiple target since we have multiple tasks and multiple head)
-        
-        
-        target = self.targets[idx]
-        input_tensor = torch.tensor(input, dtype=torch.float32)
-        target_tensor = torch.tensor(target, dtype=torch.float32)
-        return input_tensor, target_tensor
+        target = torch.zeros(self.num_tasks * 2, self.sequence_length, 2)
+
+        name_chr = self.targets[0][chr_idx]
+        for task in range(self.num_tasks):
+            # get profile shape for both positive and negative strands
+            ps_pos = torch.Tensor(
+                self.targets[1][task].values(name_chr, start, end))
+            ps_neg = torch.Tensor(
+                self.targets[2][task].values(name_chr, start, end))
+            ps_pos[ps_pos != ps_pos] = 0
+            ps_neg[ps_neg != ps_neg] = 0
+            target[task * 2] = torch.stack([ps_pos, ps_neg], dim=1)
+
+            # get total counts
+            tc_pos = ps_pos.sum()
+            tc_neg = ps_neg.sum()
+            target[task * 2 + 1][0] = torch.Tensor([tc_pos, tc_neg])
+
+        return input, target
