bed265a029e52c4c6f29964cbf159bc198d3e2bc
macwiatrak <macwiatrak@gmail.com>
2023-12-04 16:30:41 +0000

    feat: init working on the BPNetWith ProteinEmbeddings

--- model.py
+++ model.py
@@ -1,12 +1,10 @@
+import torch
 import torch.nn as nn
 
 
-class BPNet(nn.Module):
-    def __init__(self, num_tasks):
-        super(BPNet, self).__init__()
-        self.num_tasks = num_tasks
-
-        # Body (Convolutional Layers)
+class ConvEncoder(nn.Module):
+    def __init__(self):
+        super(ConvEncoder, self).__init__()
         self.conv1 = nn.Conv1d(4, 64, kernel_size=25, padding="same")
         self.relu = nn.ReLU()
         self.conv_layers = nn.ModuleList(
@@ -15,6 +13,20 @@ class BPNet(nn.Module):
                 for i in range(1, 10)
             ]
         )
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x.permute(0, 2, 1)))
+        for conv_layer in self.conv_layers:
+            x = self.relu(conv_layer(x))
+        return x
+
+
+class BPNet(nn.Module):
+    def __init__(self, num_tasks):
+        super(BPNet, self).__init__()
+        self.num_tasks = num_tasks
+
+        self.conv_layers = ConvEncoder()
 
         # Heads (Output Layers)
         self.profile_shape_heads = nn.ModuleList(
@@ -38,9 +50,7 @@ class BPNet(nn.Module):
 
     def forward(self, x):
         # Body
-        x = self.relu(self.conv1(x.permute(0, 2, 1)))
-        for conv_layer in self.conv_layers:
-            x = self.relu(conv_layer(x))
+        x = self.conv_layers(x)
 
         # Profile Shape and Total Counts Heads
         outputs = []
@@ -55,3 +65,46 @@ class BPNet(nn.Module):
             outputs.append(cx)  # total counts output appended
 
         return outputs
+
+
+class BPNetWithProteinEmbeddings(nn.Module):
+    def __init__(self, num_tasks, protein_embedding_dim=256):
+        super(BPNetWithProteinEmbeddings, self).__init__()
+        self.num_tasks = num_tasks
+        self.protein_embedding_dim = protein_embedding_dim
+
+        self.conv_layers = ConvEncoder()
+        # TODO: potentially replace with a flattening layer instead of ConvTranspose2d
+        self.conv_transpose = nn.ConvTranspose2d(
+            64, 1, kernel_size=(25, 1), padding=(12, 0))
+
+        self.protein_embedder = nn.Sequential(
+            nn.Linear(protein_embedding_dim, 1000),
+            nn.LayerNorm(1000),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+        )
+        self.profile_head = nn.Linear(1000, 2)
+        self.total_counts_head = nn.Linear(1000, 2)
+
+    def forward(self, dna_seq: torch.Tensor, prot_embeddings: torch.Tensor):
+        # DNA sequence
+        dna_emb = self.conv_layers(dna_seq)
+        dna_emb = self.conv_transpose(dna_emb.unsqueeze(3)).squeeze(-1).permute(0, 2, 1)
+
+        # Protein embeddings
+        prot_emb = self.protein_embedder(prot_embeddings)
+
+        # TODO: implement cross-attention here
+        # TODO: repeat the dna_emb to match the number of unique prot_emb
+        # TODO: calculate cross-attention
+        batch_size, dim = dna_emb.shape
+        n_prot, _ = prot_emb.shape
+        prot_dna_cross_att_output = torch.randn(batch_size, n_prot, dim)
+
+        # final layers
+        profile_shape_output = self.profile_head(prot_dna_cross_att_output)
+        total_counts_output = self.total_counts_head(prot_dna_cross_att_output)
+
+        # TODO: adapt the outputs to be similar as the BPNet above
+        return
